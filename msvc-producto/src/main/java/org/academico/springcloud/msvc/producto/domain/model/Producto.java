c85569539ab8ba771e526bb0791d2543df76b207
Eithan120 <eatorresedi@gmail.com>
2025-08-20 14:59:29 -0500

    Crear entidad producto

---
+++ msvc-producto/src/main/java/org/academico/springcloud/msvc/producto/domain/model/Producto.java
@@ -0,0 +1,67 @@
+package org.academico.springcloud.msvc.producto.domain.model;
+
+public class Producto {
+    private Long productoId;
+    private String nombre;
+    private String marca;
+    private int talla;
+    private String color;
+    private String descripcion;
+    private Double precio;
+
+    public Long getProductoId() {
+        return productoId;
+    }
+
+    public void setProductoId(Long productoId) {
+        this.productoId = productoId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getTalla() {
+        return talla;
+    }
+
+    public void setTalla(int talla) {
+        this.talla = talla;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+}
